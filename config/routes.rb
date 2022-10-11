2445a117786efed90c7420496081c1a8f2427520
Emily Port <emilylport@gmail.com>
2022-10-11 16:36:21 -0600

    Add input fields to new view for apartments

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/apartments', to: 'apartments#index'
+  get '/apartments/new', to: 'apartments#new'
+  post '/apartments', to: 'apartments#create'
 end
