134d6269dbc3bd7b9d10409de19fb07a639139a4
Emily Port <emilylport@gmail.com>
2022-10-16 16:34:22 -0600

    add delete button for each tenant on tenant index page

--- spec/features/tenants/index_spec.rb
+++ spec/features/tenants/index_spec.rb
@@ -35,6 +35,18 @@ RSpec.describe 'tenants index page', type: :feature do
 
       end
 
+      it 'i can click on a link to delete tenant and return to index page where tenant is not listed' do 
+        apartment_1 = Apartment.create!(apt_name: "Riverview Apartments", has_wd: true, unit_count: 100, city: "Saint Paul", state: "MN", pet_friendly: true)        
+        tenant_1 = apartment_1.tenants.create!(tenant_name: "Stevie Nicks", income: 100000, pets: true, occupation: "singer", rent_price: 2500, apartment_id: 1)
+        visit '/tenants'
+        expect(page).to have_content("#{tenant_1.tenant_name}")
+        click_button "Delete #{tenant_1.tenant_name}"
+        expect(current_path).to eq('/tenants')
+        expect(page).to_not have_content("#{tenant_1.tenant_name}")
+
+
+      end
+
 
     end
 
