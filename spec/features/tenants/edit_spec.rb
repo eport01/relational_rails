c35d976c076c02e81faaab9b311aa23aaf93ebc7
Emily Port <emilylport@gmail.com>
2022-10-16 14:11:59 -0600

    add more tests for tenant edit page

--- spec/features/tenants/edit_spec.rb
+++ spec/features/tenants/edit_spec.rb
@@ -10,12 +10,23 @@ RSpec.describe 'tenants update page' do
     end
     it 'when i click the link on show page it takes me to the edit page' do 
       visit "/tenants/#{@tenant_1.id}"
+      expect(page).to have_content('2500')
       click_link 'Update this Tenant'
       expect(current_path).to eq("/tenants/#{@tenant_1.id}/edit")
     end
 
     it 'after i edit the attrubutes i click submit and i am redirected to tenant show page where i see new info' do 
+      visit "/tenants/#{@tenant_1.id}/edit"
+      fill_in :tenant_name, with: 'Stevie Nicks'
+      fill_in :income, with: 100000
+      # choose  :pets, with: true   
+      fill_in :occupation, with: 'singer'
+      fill_in :rent_price, with: 2000
+      click_button 'Update this Tenant'
 
+      visit "/tenants/#{@tenant_1.id}"
+      expect(page).to have_content('2000')
+      expect(page).to_not have_content('2500')
     end
   end
 end
